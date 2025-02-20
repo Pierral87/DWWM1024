74afd2b4a66905a1f296f4a08d1878cfa4075be8
Pierral87 <pa.lacaze1@gmail.com>
2025-02-20 12:29:08 +0100

    Symfony BiblioSF6 Crud Abonne complete

--- Symfony/BiblioSF6/src/Controller/AbonneController.php
+++ Symfony/BiblioSF6/src/Controller/AbonneController.php
@@ -9,28 +9,29 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 // EXERCICE 
 // Faire fonctionner le crud automatique Abonne
 // Bloquer l'accès à ce controller uniquement pour les admin et biblio
-                // Vous avez dans le security.yaml les access control, en rajoutant ici /admin devant chaque route 
+// Vous avez dans le security.yaml les access control, en rajoutant ici /admin devant chaque route 
 // Enlever la lisibilité du password dans l'index et le show 
-                // Supprimer la colonne et la cellule qui correspond à chaque tour de boucle
+// Supprimer la colonne et la cellule qui correspond à chaque tour de boucle
 // Corriger les erreurs m'empechant de faire un edit et un new abonné (réglage du form)
-                // Erreur en rapport avec le roles qui est un champ de type array, donc il faut le régler en mettant un ChoiceType dans le form, réglé en multiple => true et expanded => true
-                // Si on reste tel quel, les nouvelles insertions et modifications d'abonnés n'auront pas le password hashé !!!! 
-                // Dans le AbonneFormType Il faut mapped false le password (comme le registrationformtype)
-                // Ensuite, dans le controller à la route new et edit, il faut récupérer ce password clair pour le hasher et le set dans l'entity avec de la persist/flush
+// Erreur en rapport avec le roles qui est un champ de type array, donc il faut le régler en mettant un ChoiceType dans le form, réglé en multiple => true et expanded => true
+// Si on reste tel quel, les nouvelles insertions et modifications d'abonnés n'auront pas le password hashé !!!! 
+// Dans le AbonneFormType Il faut mapped false le password (comme le registrationformtype)
+// Ensuite, dans le controller à la route new et edit, il faut récupérer ce password clair pour le hasher et le set dans l'entity avec de la persist/flush
 // Plutôt que d'afficher ["ROLE_USER", "ROLE_ADMIN"], on aimerait plutôt afficher des mots simples
-    // ROLE_ADMIN => Directeur
-    // ROLE_BIBLIO => Bibliothécaire
-    // ROLE_USER / ROLE_LECTEUR => Lecteur 
-    // Une boucle à gérer sur les roles user pour afficher un mot à la condition de chacun des roles 
+// ROLE_ADMIN => Directeur
+// ROLE_BIBLIO => Bibliothécaire
+// ROLE_USER / ROLE_LECTEUR => Lecteur 
+// Une boucle à gérer sur les roles user pour afficher un mot à la condition de chacun des roles 
 
 
-    // ici 
+// ici 
 #[Route('/admin/abonne')]
 final class AbonneController extends AbstractController
 {
@@ -43,13 +44,24 @@ final class AbonneController extends AbstractController
     }
 
     #[Route('/new', name: 'app_abonne_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $abonne = new Abonne();
         $form = $this->createForm(AbonneType::class, $abonne);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // dd($abonne);
+            // Ici je vais récupérer le password clair pour pouvoir le hash!
+            $password = $form->get("password")->getData();
+            // dd($password);
+            // Je hash le password 
+            // J'ai besoin ici d'appeler une dépendance de hash de password, il en existe plusieurs, ici on utilise UserPasswordHasherInterface
+            $password = $userPasswordHasher->hashPassword($abonne, $password);
+            // dd($password);
+            // J'insère dans l'entité abonne, grâce au setter, le nouveau password hashé, l'entité est donc complète je peux l'envoyer à la bdd
+            $abonne->setPassword($password);
+
             $entityManager->persist($abonne);
             $entityManager->flush();
 
@@ -71,12 +83,23 @@ final class AbonneController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_abonne_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Abonne $abonne, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Abonne $abonne, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(AbonneType::class, $abonne);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+
+            // Ici ce if me sert à hash le password uniquement s'il y a une saisie dans l'input password, sinon, le champ serait null et le hash déclencherait une erreur 
+            if ($password = $form->get("password")->getData()) {
+                $password = $userPasswordHasher->hashPassword($abonne, $password);
+
+                $abonne->setPassword($password);
+            }
+
+
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_abonne_index', [], Response::HTTP_SEE_OTHER);
@@ -91,7 +114,7 @@ final class AbonneController extends AbstractController
     #[Route('/{id}', name: 'app_abonne_delete', methods: ['POST'])]
     public function delete(Request $request, Abonne $abonne, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$abonne->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $abonne->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($abonne);
             $entityManager->flush();
         }
--- Symfony/BiblioSF6/src/Form/AbonneType.php
+++ Symfony/BiblioSF6/src/Form/AbonneType.php
@@ -4,17 +4,52 @@ namespace App\Form;
 
 use App\Entity\Abonne;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class AbonneType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        // Récupération dans la variable $abonne d'une des options passé en param, lors du edit on peut 
+        // dd($options);
+        $abonne = $options["data"];
+
         $builder
             ->add('pseudo')
-            ->add('roles')
-            ->add('password')
+            // Ici si je laisse uniquement le add("roles") sans aucun type, il va penser que c'est un input text (type par defaut)
+            // Malheureusement, par rapport au mapping de l'entité, j'attends ici un array (format json dans la table), donc je dois le régler en le mettant en ChoiceType
+            // Et surtout en multiple true 
+            ->add('roles', ChoiceType::class, [
+                "choices" => [
+                    "Directeur" => "ROLE_ADMIN",
+                    "Bibliothecaire" => "ROLE_BIBLIO",
+                    "Lecteur" => "ROLE_LECTEUR"
+                ],
+                // multiple true, c'est laisser la possibilité de plusieurs valeurs dans le champs en question
+                "multiple" => true,
+                // expanded pour afficher une liste ici en checkbox
+                "expanded" => true
+            ])
+            ->add('password', TextType::class, [
+                // Ici pour le password je met mapped false, sinon, ce champ est rattaché directement à notre entité et les password seront alors enregistrés en clair ! Je dois donc le "détacher" de l'entité, pour le manipuler, pour ensuite dans mon controller, utiliser le setter pour lui transmettre le password hashé
+                "mapped" => false,
+                // "constraints" => [
+                //     new Regex([
+                //         // Ici un pattern forçant une min, une maj, un chiffre, un caractère spécial, entre 6 et 10 carac
+                //         "pattern" => "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[-+!*$@%_])([-+!*$@%_\w]{6,10})$/",
+                //         "message" => "Attention votre mdp ne correspond pas au pattern, au moins 1 min 1 maj 1 chiffre 1 carac spécial et faire entre 6 et 10"
+                //     ])
+                // ]
+                // Ici, grâce à l'entité $abonne que j'ai récupérée via les options, je peux vérifier si un id existe dans l'entité !
+                // S'il n'y a pas d'id, c'est à dire que je suis dans un contexte d'insertion (l'id est null en attendant la première insertion), donc le champ password est required ! Par contre, si getId() me retourne bien un id, alors l'entité possède un id, donc elle existe déjà en table de données, DONC je suis dans un contexte de modification, le champ n'est plus required
+                "required" => $abonne->getId() ? false : true
+            ])
             ->add('prenom')
             ->add('nom')
         ;
